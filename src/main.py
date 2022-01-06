a619c3d255cd7b30c175a544e42c5179ab38204c
3top1a <57371001+3top1a@users.noreply.github.com>
2022-01-06 13:35:06 +0100

    pepe8 formatting

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ def get_token(server, password, username):
     access_token = data.get('access_token')
     return access_token
 
+
 def get_timetable_data(server, token):
     _url = server + "/api/3/timetable/actual"
     header = {'Content-Type': 'application/x-www-form-urlencoded',
@@ -43,9 +44,10 @@ def get_timetable_data(server, token):
     day_table = []
     for x in day_table_raw:
         for y in x['Atoms']:
-            day_table.append( [ x['DayOfWeek'], y['HourId'], y['SubjectId'], y['TeacherId'], y['Theme'], y['RoomId'] ] )
-    
-    #Make a nice list of period starts and ends. { Id : [ Caption, BeginTime, EndTime] }
+            day_table.append([x['DayOfWeek'], y['HourId'], y['SubjectId'],
+                             y['TeacherId'], y['Theme'], y['RoomId']])
+
+    # Make a nice list of period starts and ends. { Id : [ Caption, BeginTime, EndTime] }
     hours_raw = data.get('Hours')
     hours = {}
     for x in hours_raw:
@@ -53,10 +55,11 @@ def get_timetable_data(server, token):
 
     return day_table, subject_names, hours
 
-def display_timetable_data(data):
+
+def display_timetable_data_table(data):
     # Default table display like in the web version
 
-    table_raw = data[0] 
+    table_raw = data[0]
     subjects = data[1]
     hours = data[2]
     table = [[]]
@@ -65,7 +68,7 @@ def display_timetable_data(data):
     # Add all the begin times (7:00 - 7:45, 7:50, ...)
     for x in hours:
         headers.append(hours[x][1] + "\n" + hours[x][2])
-    
+
     for x in table_raw:
         name = '' if subjects.get(x[2]) == None else subjects.get(x[2])[0]
         day = x[0]
@@ -77,8 +80,8 @@ def display_timetable_data(data):
                 table[day].append('  ')
         table[day][x[1]] = name
 
-    table.remove(table[0]) # Since day starts at 1 by default
-    
+    table.remove(table[0])  # Since day starts at 1 by default
+
     # Save the modified data seperately
     mod_headers = []
     mod_table = []
@@ -96,36 +99,39 @@ def display_timetable_data(data):
             column.append(x)
 
         # Add if not empty
-        if( column.count('  ') != len(column)):
+        if(column.count('  ') != len(column)):
             mod_headers.append(header)
-            
+
             for day in table:
                 if len(mod_table) == table.index(day):
                     mod_table.append([])
                 mod_table[table.index(day)].append(day[headerindex])
 
-    print(tabulate(mod_table, headers = mod_headers, tablefmt="simple"))
-    #print(mod_table)
+    print(tabulate(mod_table, headers=mod_headers, tablefmt="simple"))
+    # print(mod_table)
+
 
 def timetable(server, password, username):
     token = get_token(server, password, username)
-    display_timetable_data(get_timetable_data(server, token))
+    display_timetable_data_table(get_timetable_data(server, token))
 
 # Parsing
+
+
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], "hs:p:u:t", ["server", "password", "username", "homework"])
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "hs:p:u:t", [
+                                       "server", "password", "username", "homework"])
     except getopt.GetoptError:
-        print ('main.py -s example.bakalari.cz -p password -u username')
+        print('main.py -s example.bakalari.cz -p password -u username')
         sys.exit(2)
-    server   = ""
+    server = ""
     password = ""
-    username = ""           
-
+    username = ""
 
     for opt, arg in opts:
         if opt == '-h':
-            print ('main.py -s example.bakalari.cz -p password -u username')
+            print('main.py -s example.bakalari.cz -p password -u username')
             sys.exit()
         elif opt in ("-s", "--server"):
             server = arg
@@ -135,9 +141,7 @@ def main():
             username = arg
 
     timetable(server, password, username)
-    
-    
 
 
 if __name__ == "__main__":
-   main()
+    main()
